2a9422d928fbd631e02c3388ca56dac4c58b008e
imar759 <imar759@aucklanduni.ac.nz>
2019-03-28 21:26:53 +1300

    Change style and make entering text easier

--- waypoint_server/scripts/text_box_input.py
+++ waypoint_server/scripts/text_box_input.py
@@ -1,45 +1,56 @@
 #!/usr/bin/python2
 
 from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtCore import QSize    
+from PyQt5.QtWidgets import QPushButton, QDesktopWidget
+from PyQt5.QtCore import QSize
 
-class MainWindow(QMainWindow):
+class InputWindow(QMainWindow):
     def __init__(self, waypoint_name):
         QMainWindow.__init__(self)
 
-
-        self.setMinimumSize(QSize(300, 100))    
-        self.setWindowTitle("Rename Waypoint") 
+        self.setMinimumSize(QSize(300, 100))
+        self.setWindowTitle("Rename Waypoint")
 
         self.current_name = waypoint_name
         self.nameLabel = QLabel(self)
-        self.line = QLineEdit(self)
-        self.line.setText(self.current_name)
-
-        self.line.move(50, 20)
-        self.line.resize(200, 32)
         self.nameLabel.move(20, 20)
 
-        renameButton = QPushButton('OK', self)
-        cancelButton = QPushButton('Cancel', self)
-        renameButton.clicked.connect(self.renameMethod)
-        cancelButton.clicked.connect(self.cancelMethod)
-        renameButton.resize(80,32)
-        renameButton.move(160, 60)
-        cancelButton.resize(80,32) 
-        cancelButton.move(60, 60)       
+        self.input_line = QLineEdit(self)
+        self.input_line.setText(self.current_name)
+        self.input_line.selectAll()
+        self.input_line.returnPressed.connect(self.okMethod)
+        self.input_line.resize(200, 32)
+        self.input_line.move(50, 20)
 
-    def renameMethod(self):
-        self.current_name = self.line.text()
+        okButton = QPushButton('OK', self)
+        okButton.clicked.connect(self.okMethod)
+        okButton.setAutoDefault(True)
+        okButton.setDefault(True)
+        okButton.resize(80,32)
+        okButton.move(160, 60)
+        okButton.setAutoDefault(True)
+
+        cancelButton = QPushButton('Cancel', self)
+        cancelButton.clicked.connect(self.cancelMethod)
+        cancelButton.resize(80,32)
+        cancelButton.move(60, 60)
+
+        # center window
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        print centerPoint
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+
+    def okMethod(self):
+        self.current_name = self.input_line.text().encode('utf8')
         self.close()
 
     def cancelMethod(self):
-        self.current_name = "potato"
         self.close()
 
     def getNewName(self):
         return self.current_name
 
 
-        
+
